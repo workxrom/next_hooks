d3b6958785bde4f8cca1d663baa8461d5d5796c6
Nikita <xpom5566@mail.ru>
2019-05-20 10:54:04 +0300

    + hooks addTen

--- components/Increment.js
+++ components/Increment.js
@@ -2,13 +2,16 @@ import { useState } from 'react';
 
 const Increment = () => {
   const [count, setCount] = useState(0);
+  const [addTen, setAddTen] = useState(0);
 
-  const onClick = () => setCount(count + 1);
+  const onClickOne = () => setCount(count + 1);
+  const onClickTen = () => setAddTen(addTen + 10);
 
   return (
     <div>
-      <h1>`count = ${count}`</h1>
-      <button onClick={onClick}>Add</button>
+      <h1>`count = ${count + addTen}`</h1>
+      <button onClick={onClickOne}>+1</button>
+      <button onClick={onClickTen}>+10</button>
     </div>
   );
 };
